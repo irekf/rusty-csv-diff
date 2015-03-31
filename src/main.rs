2d23e280b10062d9baef2e9fedb447db5cbcab74
Irek Fakhrutdinov <irekfakh@gmail.com>
2015-03-31 22:17:47 +0500

    Write down the algorithm(this algorithm is similar to one I used before to compare two CSV files)

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,29 @@
-#![feature(old_path)]
-#![feature(io)]
-
 use std::env;
-use std::io::prelude::*;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+use std::str::FromStr;
 
 fn main() {
 
-    // get the arguments
-    let arguments: Vec<String> = env::args().collect();
+/*
 
-    // csv file path
-    let path_string: String = arguments[1].clone();
-    let csv_path: Path = Path::new(path_string);
+1. Parse arguments
+2. Open CSV files
+3. Get columns (cols_N)
+4. Get intersection of those two sets of columns(cols_to_compare)
+5. Create {column name : column index in cols_N} dicts
+6. Create {CSV_col_value : CSV row index in file} dicts, where CSV_col_value is a unique key made of the value of several CSV columns.
+   For example, {Alex38 : 76}. In this example a name and age form a unique key for the 76th CSV row.
+7. Get intersection of key sets of dicts from step 6 (row_keys_to_compare)
+8. Loop through row_keys_to_compare, use dicts from step 6 to get line numbers for CSV files
+    8.1 Loop through cols_to_compare, use dicts from step 5 to extract column values from CSV rows(step 8)
+    8.2 Compare values
 
-    // let's open an csv file
-    let mut csv_file = match File::open(&csv_path) {
-        Err(why)     => panic!("error openning csv file: {}", why.description()),
-        Ok(csv_file) => csv_file,
-    };
+Input parameters: CSV paths, delimiters, quotes
 
-    // reader
-    let mut csv_reader = BufReader::new(csv_file);
-    let mut line_buffer: String = String::new();
+For example, ./main file_1.csv "," "'" file_2.csv " " "" 
 
-    loop {
-        match csv_reader.read_line(&mut line_buffer) {
-            Err(why) => panic!("error reading from file: {}", why.description()),
-            Ok(lines_read) =>   { 
-                                  println!("file has been read, lines read = {:?}", lines_read);
-                                }
-        };
-        println!("{}", line_buffer);
-    };
+*/
+
 }
