5a0fd30f08d942bf3bcfc1c45ad3d8c7ba550e67
Irek Fakhrutdinov <irekfakh@gmail.com>
2015-04-15 23:24:24 +0500

    Use argparse instead of optparse. optparse is deprecated.

--- src/csv_tools/main.py
+++ src/csv_tools/main.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
-from optparse import OptionParser
 
+import argparse
 import csvgen
 import shuffle
 import sys
@@ -9,22 +9,22 @@ import sys
 TOOL_GEN     = "gen"
 TOOL_SHUFFLE = "shuffle"
 
-def generate_csv(options, args):
+def generate_csv(args):
     print("generating a CSV file...")
-    path = options.out_file_name
-    row_num = options.row_num
-    col_num = options.col_num
-    delim = options.delim
-    quote = options.quote
+    path = args.output_file
+    row_num = args.row_num
+    col_num = args.col_num
+    delim = args.delim
+    quote = args.quote
     csvgen.generate_csv(row_num, col_num, path, delim, quote)
 
-def shuffle_csv(options, args):
+def shuffle_csv(args):
     print("shuffling a CSV file...")
-    input_path = options.input_file_name
-    output_path = options.output_file_name
+    input_path = args.input_file
+    output_path = args.output_file
     shuffle.shuffle_csv(input_path, output_path)
 
-def unknown_tool(options, args):
+def unknown_tool(args):
     print("unknown tool found, please use either \"gen\" or \"shuffle\"")
 
 def get_default_tool():
@@ -33,67 +33,40 @@ def get_default_tool():
 tools = defaultdict(get_default_tool, {TOOL_GEN : generate_csv,
                     TOOL_SHUFFLE : shuffle_csv})
 
-def get_opt_parser(tool):
+def get_arg_parser(tool):
 
-    parser = OptionParser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("tool", type=str,
+                        help="CSV tool, [gen | shuffle]")
 
     if tool == TOOL_GEN:
-        parser.add_option("-o", "--output",
-                action="store", type="string", dest="out_file_name",
-                help="name of output CSV file")
-        parser.add_option("-r", "--row_number",
-                action="store", type="int", dest="row_num",
-                help="number of rows in output CSV file")
-        parser.add_option("-c", "--col_number",
-                action="store", type="int", dest="col_num",
-                help="number of columns in output CSV file")
-        parser.add_option("-d", "--delimiter",
-                action="store", type="string", dest="delim",
+        parser.add_argument("row_num", type=int, help="number of rows in CSV")
+        parser.add_argument("col_num", type=int, help="number of columns in CSV")
+        parser.add_argument("output_file", type=str, help="name of output file")
+        parser.add_argument("-d", "--delimiter",
+                action="store", type=str, dest="delim",
                 help="delimiter used in  output CSV file")
-        parser.add_option("-q", "--quote",
-                action="store", type="string", dest="quote",
+        parser.add_argument("-q", "--quote",
+                action="store", type=str, dest="quote",
                 help="quote used in  output CSV file")
     elif tool == TOOL_SHUFFLE:
-        parser.add_option("-i", "--input",
-                action="store", type="string", dest="input_file_name",
-                help="name of input CSV file")
-        parser.add_option("-o", "--output",
-                action="store", type="string", dest="output_file_name",
-                help="name of output CSV file")
+        parser.add_argument("input_file", type=str, help="name of input CSV file")
+        parser.add_argument("output_file", type=str, help="name of output CSV file")
     else:
         pass
 
     return parser
-
-def parse_args(tool_name, opt_parser, args):
-    (options, args) = opt_parser.parse_args(args)
-    if tool_name == TOOL_GEN:
-        if options.out_file_name is None:
-            opt_parser.error("output file name not given")
-        if options.row_num is None:
-            opt_parser.error("row number not given")
-        if options.col_num is None:
-            opt_parser.error("col_number not fiven")
-    elif tool_name == TOOL_SHUFFLE:
-        if options.input_file_name is None:
-            opt_parser.error("input file name not given")
-        if options.output_file_name is None:
-            opt_parser.error("output file name not given")
-    else:
-        pass
-
-    return (options, args)
 
 def main():
 
     tool_name = sys.argv[1]
     args = sys.argv[2:]
 
-    opt_parser = get_opt_parser(tool_name)
+    arg_parser = get_arg_parser(tool_name)
 
-    (options, args) = parse_args(tool_name, opt_parser, args)
+    args = arg_parser.parse_args()
 
-    tools[tool_name](options, args)
+    tools[tool_name](args)
 
 if __name__ == "__main__":
     main()
